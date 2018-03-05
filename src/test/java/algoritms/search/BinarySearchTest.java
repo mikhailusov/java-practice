7a791e0d1c0a498787765b94502974ed626119cb
Mikhail Usov <musovx@gmail.com>
2018-03-04 16:16:56 -0800

    Binary Search Implementation

--- src/test/java/algoritms/search/BinarySearchTest.java
+++ src/test/java/algoritms/search/BinarySearchTest.java
@@ -8,9 +8,12 @@ public class BinarySearchTest {
 
     @Test
     public void search() {
+        assertEquals(0, (int) BinarySearch.search(new int[]{-3,-1,1,5,55,122}, -3));
         assertEquals(3, (int) BinarySearch.search(new int[]{-3,-1,1,5,55,122}, 5));
+        assertEquals(5, (int) BinarySearch.search(new int[]{-3,-1,1,5,55,122}, 122));
         assertEquals(0, (int) BinarySearch.search(new int[]{5}, 5));
         assertNull(BinarySearch.search(new int[]{-3,-1,1,5,55,122}, 6));
         assertNull(BinarySearch.search(new int[]{}, 6));
+        assertNull(BinarySearch.search(new int[]{1,5,9}, 6));
     }
 }
