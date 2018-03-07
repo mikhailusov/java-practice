f94e03dc2c765cd967b8dff71c6a8ad9cde055f1
Mikhail Usov <musovx@gmail.com>
2018-03-06 21:37:50 -0800

    Add Prime checker

---
+++ src/test/java/algoritms/other/PrimeCheckerTest.java
@@ -0,0 +1,24 @@
+package algoritms.other;
+
+import org.junit.Test;
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+public class PrimeCheckerTest {
+
+    @Test
+    public void isPrime() {
+        assertTrue(PrimeChecker.isPrime(2));
+        assertTrue(PrimeChecker.isPrime(3));
+        assertTrue(PrimeChecker.isPrime(5));
+        assertTrue(PrimeChecker.isPrime(7));
+        assertTrue(PrimeChecker.isPrime(11));
+        assertFalse(PrimeChecker.isPrime(0));
+        assertFalse(PrimeChecker.isPrime(-2));
+        assertFalse(PrimeChecker.isPrime(-7));
+        assertFalse(PrimeChecker.isPrime(1));
+        assertFalse(PrimeChecker.isPrime(4));
+        assertFalse(PrimeChecker.isPrime(9));
+        assertFalse(PrimeChecker.isPrime(99));
+    }
+}
