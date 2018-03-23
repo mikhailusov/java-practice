71e0229fda25f3ab1148e137a83d4f27abe45d0d
Mikhail Usov <musovx@gmail.com>
2018-03-22 21:54:25 -0700

    Adding binary tree traversals recursive + iterative. Initial version.

---
+++ src/main/java/algoritms/tree/BinaryTreeTraversals.java
@@ -0,0 +1,168 @@
+package algoritms.tree;
+
+import problems.trees.TreeNode;
+import java.util.Stack;
+
+public class BinaryTreeTraversals {
+
+    private static TreeNode tree;
+
+    public static void preOrderRecursive(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        print(root);
+        preOrderRecursive(root.left);
+        preOrderRecursive(root.right);
+    }
+
+    public static void preOrderIterative(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            print(node);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+
+        }
+    }
+
+    public static void inOrderRecursive(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        inOrderRecursive(root.left);
+        print(root);
+        inOrderRecursive(root.right);
+    }
+
+    public static void inOrderIterative(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode node = root;
+
+        while (node != null) {
+            stack.push(node);
+            node = node.left;
+        }
+
+        while (!stack.isEmpty()) {
+            node = stack.pop();
+            print(node);
+            if (node.right != null) {
+                node = node.right;
+                while (node != null) {
+                    stack.push(node);
+                    node = node.left;
+                }
+            }
+        }
+    }
+
+    public static void postOrderRecursive(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        postOrderRecursive(root.left);
+        postOrderRecursive(root.right);
+        print(root);
+    }
+
+    public static void postOrderIterative(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        Stack<TreeNode> stack1 = new Stack<>();
+        Stack<TreeNode> stack2 = new Stack<>();
+
+        stack1.push(root);
+
+        while (!stack1.empty()) {
+            TreeNode node = stack1.pop();
+            stack2.push(node);
+            if (node.left != null) {
+                stack1.push(node.left);
+            }
+            if (node.right != null) {
+                stack1.push(node.right);
+            }
+        }
+
+        while (!stack2.isEmpty()) {
+            print(stack2.pop());
+        }
+    }
+
+    public static void main(String[] args) {
+        // pre-orders
+        preOrderRecursive(generateTree());
+        System.out.println();
+        preOrderIterative(generateTree());
+
+        System.out.println();
+        System.out.println();
+
+        // in-orders
+        inOrderRecursive(generateTree());
+        System.out.println();
+        inOrderIterative(generateTree());
+
+        System.out.println();
+        System.out.println();
+
+        // post-orders
+        postOrderRecursive(generateTree());
+        System.out.println();
+        postOrderIterative(generateTree());
+
+
+    }
+
+    /**
+     *     5
+     *   /   \
+     *  3     6
+     * / \     \
+     * 2  1     7
+     *
+     */
+    private static TreeNode generateTree() {
+        if (tree != null) {
+            return tree;
+        }
+        TreeNode root = new TreeNode(5);
+        TreeNode node1 = new TreeNode(3);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(1);
+        TreeNode node4 = new TreeNode(6);
+        TreeNode node5 = new TreeNode(7);
+
+        root.left = node1;
+        root.right = node4;
+        node1.left = node2;
+        node1.right = node3;
+        node4.right = node5;
+
+        return root;
+    }
+
+    private static void print(TreeNode node) {
+        System.out.print(node.val + " ");
+    }
+}
--- src/main/java/problems/trees/TreeNode.java
+++ src/main/java/problems/trees/TreeNode.java
@@ -1,8 +1,8 @@
 package problems.trees;
 
 public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode(int x) { val = x; }
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val) { this.val = val; }
 }
